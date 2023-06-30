2fe9bd0e1c5eb054f0ea59c126ecdc6b452a35bd
Sonal Agrawal <sonalfeb7@gmail.com>
2023-06-30 12:47:39 +0530

    Adding Users dynamic in lists

--- index.js
+++ index.js
@@ -1,19 +1,30 @@
+var ul = document.querySelector('.lists')
 function UserForm(e) {
   e.preventDefault();
 
-  const name = document.getElementById("name").value;
+  const name = document.getElementById("name");
 
-  const email = document.getElementById("email").value;
+  const email = document.getElementById("email")
 
-  const number = document.getElementById("number").value;
+  const number = document.getElementById("number")
 
-  const date = document.getElementById("date").value;
+  const date = document.getElementById("date")
 
-  const time = document.getElementById("time").value;
+  const time = document.getElementById("time")
 
-  if (name == "" || email == "" || number == "" || date == "" || time == "") {
-    console.log("Please fill all inputs");
+  if (name.value == "" || email.value == "" || number.value == "" || date.value == "" || time.value == "") {
+    var msg = document.getElementById('errorMsg');
+    msg.style.display ="block";
+    setTimeout(()=>msg.style.display="none",3000);
   } else {
-    console.log(name, email, number, date, time);
+    var li = document.createElement('li');
+    var text = document.createTextNode(`${name.value} : ${email.value} : ${number.value} : ${date.value} : ${time.value}`);
+    li.appendChild(text);
+    ul.appendChild(li)
+    name.value ='';
+    email.value='';
+    number.value='';
+    date.value='';
+    time.value='';
   }
 }
