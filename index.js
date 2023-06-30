74012d2c0eb5a272de0e9f227a67f6e2be4b9bae
Sonal Agrawal <sonalfeb7@gmail.com>
2023-06-30 18:21:22 +0530

    Added Edit Functionality

--- index.js
+++ index.js
@@ -1,38 +1,43 @@
+const Uname = document.getElementById("name");
+
+const email = document.getElementById("email");
+
+const number = document.getElementById("number");
+
+const date = document.getElementById("date");
+
+const time = document.getElementById("time");
+const submit = document.getElementById("submit");
 var ul = document.querySelector(".lists");
 var data = JSON.parse(localStorage.getItem("lists"));
 var arrayList = [];
 
-if (data.length) {
+if (data.length) {// if list stored in localstorage
   arrayList = data;
   for (var i = 0; i < data.length; i++) {
     var li = document.createElement("li");
+    li.className = "list-group-item";
     var button = document.createElement("button");
     button.className = "btn btn-danger btn-sm float-right delete";
-    li.className = "list-group-item";
-    button.appendChild(document.createTextNode("Remove"));
 
-    li.textContent = data[i].name;
+    var editButton = document.createElement("button");
+    editButton.className = "btn btn-primary btn-sm float-right edit";
+    editButton.appendChild(document.createTextNode("Edit"));
+    button.appendChild(document.createTextNode("Remove"));
+    li.setAttribute("key", i);
+    li.textContent = `${data[i].name} - ${data[i].email} -${data[i]
+      .number} -${data[i].date} -${data[i].time} `;
+    li.appendChild(editButton);
     li.appendChild(button);
     ul.appendChild(li);
   }
-  console.log(ul);
 }
 
 function UserForm(e) {
   e.preventDefault();
 
-  const name = document.getElementById("name");
-
-  const email = document.getElementById("email");
-
-  const number = document.getElementById("number");
-
-  const date = document.getElementById("date");
-
-  const time = document.getElementById("time");
-
   if (
-    name.value == "" ||
+    Uname.value == "" ||
     email.value == "" ||
     number.value == "" ||
     date.value == "" ||
@@ -42,27 +47,42 @@ function UserForm(e) {
     msg.style.display = "block";
     setTimeout(() => (msg.style.display = "none"), 3000);
   } else {
-    var text = document.createTextNode(
-      `${name.value} : ${email.value} : ${number.value} : ${date.value} : ${time.value}`
-    );
-    var li = document.createElement("li");
-    var button = document.createElement("button");
-    button.className = "btn btn-danger btn-sm float-right delete";
-    li.className = "list-group-item";
-    button.appendChild(document.createTextNode("Remove"));
-    li.appendChild(text);
-    li.appendChild(button);
-    ul.appendChild(li);
-    var obj = {
-      name: name.value,
-      email: email.value,
-      number: number.value,
-      date: date.value,
-      time: time.value
-    };
-    arrayList.push(obj);
-    localStorage.setItem("lists", JSON.stringify(arrayList));
-    name.value = "";
+    var getKey = submit.getAttribute("key");/// For Edit Details
+    if (getKey) {
+      data[getKey].name = Uname.value;
+      data[getKey].email = email.value;
+      data[getKey].number = number.value;
+      data[getKey].date = date.value;
+      data[getKey].time = time.value;
+      localStorage.setItem("lists",JSON.stringify(data));
+    } else { //for add new details
+      var text = document.createTextNode(
+        `${Uname.value} : ${email.value} : ${number.value} : ${date.value} : ${time.value}`
+      );
+      var li = document.createElement("li");
+      var button = document.createElement("button");
+      button.className = "btn btn-danger btn-sm float-right delete";
+      li.className = "list-group-item";
+      button.appendChild(document.createTextNode("Remove"));
+
+      var editButton = document.createElement("button");
+      editButton.className = "btn btn-primary btn-sm float-right edit";
+      editButton.appendChild(document.createTextNode("Edit"));
+      li.appendChild(text);
+      li.appendChild(editButton);
+      li.appendChild(button);
+      ul.appendChild(li);
+      var obj = {
+        name: Uname.value,
+        email: email.value,
+        number: number.value,
+        date: date.value,
+        time: time.value
+      };
+      arrayList.push(obj);
+      localStorage.setItem("lists", JSON.stringify(arrayList));
+    }
+    Uname.value = "";
     email.value = "";
     number.value = "";
     date.value = "";
@@ -70,11 +90,25 @@ function UserForm(e) {
   }
 }
 
-ul.addEventListener('click' ,RemoveItem);
+ul.addEventListener("click", RemoveEditItem);
 
-function RemoveItem(e){
-  if(e.target.classList.contains('delete')){
-      if(confirm('Are You Sure')){
-          var li = e.target.parentElement;
-          ul.removeChild(li);
-      }}}
+function RemoveEditItem(e) {
+  if (e.target.classList.contains("delete")) {
+    if (confirm("Are You Sure")) {
+      var li = e.target.parentElement;
+      ul.removeChild(li);
+    }
+  }
+  if (e.target.classList.contains("edit")) {
+    var li = e.target.parentElement;
+    var index = li.getAttribute("key");
+    var fetchdetails = JSON.parse(localStorage.getItem("lists"));
+    Uname.value = fetchdetails[index].name;
+    email.value = fetchdetails[index].email;
+    number.value = fetchdetails[index].number;
+    date.value = fetchdetails[index].date;
+    time.value = fetchdetails[index].time;
+    submit.value = "Update a call";
+    submit.setAttribute("key", index);
+  }
+}
