0ab974bd6665f670e14221146b65e5e3c7072dac
Sonal Agrawal <sonalfeb7@gmail.com>
2023-07-19 12:49:16 +0530

    Modified Some Changes and Add Get Data

--- index.js
+++ index.js
@@ -11,32 +11,34 @@ const submit = document.getElementById("submit");
 var ul = document.querySelector(".lists");
 var data = JSON.parse(localStorage.getItem("lists"));
 var arrayList = [];
+axios
+  .get(
+    "https://crudcrud.com/api/550b8d51c9eb471290dcc3d64f5f6e3b/appointmentData"
+  )
+  .then(res => {
+    if (res.data.length) {
+      for (var i = 0; i < res.data.length; i++) {
+        var li = document.createElement("li");
+        li.className = "list-group-item";
+        var button = document.createElement("button");
+        button.className = "btn btn-danger btn-sm float-right delete";
 
-if (data.length) {
-  // if list stored in localstorage
-  arrayList = data;
-  for (var i = 0; i < data.length; i++) {
-    var li = document.createElement("li");
-    li.className = "list-group-item";
-    var button = document.createElement("button");
-    button.className = "btn btn-danger btn-sm float-right delete";
-
-    var editButton = document.createElement("button");
-    editButton.className = "btn btn-primary btn-sm float-right edit";
-    editButton.appendChild(document.createTextNode("Edit"));
-    button.appendChild(document.createTextNode("Remove"));
-    li.setAttribute("key", i);
-    li.textContent = `${data[i].name} - ${data[i].email} -${data[i]
-      .number} -${data[i].date} -${data[i].time} `;
-    li.appendChild(editButton);
-    li.appendChild(button);
-    ul.appendChild(li);
-  }
-}
-
+        var editButton = document.createElement("button");
+        editButton.className = "btn btn-primary btn-sm float-right edit";
+        editButton.appendChild(document.createTextNode("Edit"));
+        button.appendChild(document.createTextNode("Remove"));
+        li.setAttribute("key", i);
+        li.textContent = `${res.data[i].name} - ${res.data[i].email} -${res
+          .data[i].number} -${res.data[i].date} -${res.data[i].time} `;
+        li.appendChild(editButton);
+        li.appendChild(button);
+        ul.appendChild(li);
+      }
+    }
+  })
+  .catch(err => console.log(err));
 function UserForm(e) {
   e.preventDefault();
-
   if (
     Uname.value == "" ||
     email.value == "" ||
@@ -48,55 +50,26 @@ function UserForm(e) {
     msg.style.display = "block";
     setTimeout(() => (msg.style.display = "none"), 3000);
   } else {
-    var getKey = submit.getAttribute("key"); /// For Edit Details
-    if (getKey) {
-      data[getKey].name = Uname.value;
-      data[getKey].email = email.value;
-      data[getKey].number = number.value;
-      data[getKey].date = date.value;
-      data[getKey].time = time.value;
-      localStorage.setItem("lists", JSON.stringify(data));
-      window.location.reload();
-    } else {
-      //for add new details
-      var text = document.createTextNode(
-        `${Uname.value} : ${email.value} : ${number.value} : ${date.value} : ${time.value}`
-      );
-      var li = document.createElement("li");
-      var button = document.createElement("button");
-      button.className = "btn btn-danger btn-sm float-right delete";
-      li.className = "list-group-item";
-      button.appendChild(document.createTextNode("Remove"));
-
-      var editButton = document.createElement("button");
-      editButton.className = "btn btn-primary btn-sm float-right edit";
-      editButton.appendChild(document.createTextNode("Edit"));
-      li.appendChild(text);
-      li.appendChild(editButton);
-      li.appendChild(button);
-      ul.appendChild(li);
-      var obj = {
-        name: Uname.value,
-        email: email.value,
-        number: number.value,
-        date: date.value,
-        time: time.value
-      };
-      arrayList.push(obj);
-      axios
-        .post(
-          "https://crudcrud.com/api/550b8d51c9eb471290dcc3d64f5f6e3b/appointmentData",
-          obj
-        )
-        .then(res => console.log(res))
-        .catch(err=>console.log(err));
-      localStorage.setItem("lists", JSON.stringify(arrayList));
-    }
-    Uname.value = "";
-    email.value = "";
-    number.value = "";
-    date.value = "";
-    time.value = "";
+    var obj = {
+      name: Uname.value,
+      email: email.value,
+      number: number.value,
+      date: date.value,
+      time: time.value
+    };
+    console.log(obj);
+    arrayList.push(obj);
+    localStorage.setItem("lists", JSON.stringify(arrayList));
+    axios
+      .post(
+        "https://crudcrud.com/api/550b8d51c9eb471290dcc3d64f5f6e3b/appointmentData",
+        obj
+      )
+      .then(res => {
+        console.log(res);
+        window.location.reload();
+      })
+      .catch(err => console.log(err));
   }
 }
 
